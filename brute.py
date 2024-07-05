503cfa9585d40c02f660ad15d56b307549114863
Zasegor <152307758+Zasegor@users.noreply.github.com>
2024-07-05 14:03:07 +0300

    brute.py
    multiprocessing

--- brute.py
+++ brute.py
@@ -1,11 +1,43 @@
+import multiprocessing as mp
+import math
+
+
+def is_prime_worker(num, start, end, result_queue):
+    for i in range(start, end):
+        if num % i == 0:
+            result_queue.put((False, i))
+            return
+    result_queue.put((True, None))
+
+
 def brute_force_primality_test(num):
     if num == 2:
-        return True
-    for i in range(2, int(pow(num, 0.5)) + 1):
+        return True, None
+    if num < 2:
+        return False, None
 
-        if num % i == 0:
-            return False
-    return True
+    num_cores = mp.cpu_count()
+    chunk_size = int(math.sqrt(num)) // num_cores
+    result_queue = mp.Queue()
+
+    processes = []
+    for i in range(num_cores):
+        start = i * chunk_size + 2
+        end = (i + 1) * chunk_size + 2 if i != num_cores - 1 else int(math.sqrt(num)) + 1
+        p = mp.Process(target=is_prime_worker, args=(num, start, end, result_queue))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    while not result_queue.empty():
+        is_prime, divisor = result_queue.get()
+        if not is_prime:
+            return False, divisor
+
+    return True, None
+
 
 
 if __name__ == "__main__":
